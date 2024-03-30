c0a74af83e7c26f4120066e54247ffa0d1f45b90
elysseaa <121833752+elysseaa@users.noreply.github.com>
2024-03-29 22:13:47 -0700

    Modified static index.html

--- scripts/index_static.js
+++ scripts/index_static.js
@@ -4,20 +4,3 @@
 
 function getId(foo) { return document.getElementById(foo); }
 function getClass(foo) { return document.getElementsByClassName(foo); }
-
-/**
- * Day/Night
- */
-
-getId('brightness').onclick = changeBrightness;
-
-function changeBrightness () {
-    var brightness = getClass('csslink')[0].href.substr(-5);
-    if (brightness == "t.css"){
-        getClass('csslink')[0].setAttribute('href', './styles/day.css'); 
-    }
-    else {
-        getClass('csslink')[0].setAttribute('href', './styles/night.css'); 
-    }
-    console.log(brightness);
-}
